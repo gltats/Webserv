209773b86586ca436669667766f62810905dc64d
gltats <tatianasofiagomes831@gmail.com>
2023-12-21 16:01:44 +0100

    error linked

---
+++ frontEnd/error/error.js
@@ -0,0 +1,4 @@
+function redirectToHome() {
+	// Use window.location.href to set the new URL
+	window.location.href = '../../frontEnd/index.html';
+  }
--- frontEnd/main.js
+++ frontEnd/main.js
@@ -10,3 +10,4 @@ for (let i = 0; i < counter; i++) {
  
   document.body.appendChild(hrElement);
 }
+
