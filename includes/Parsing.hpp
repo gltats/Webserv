2e2138c06e96eaddeb6ab8d44a5e347e49b7f408
gltats <tatianasofiagomes831@gmail.com>
2024-01-03 18:41:03 +0100

    started parsing

---
+++ includes/Parsing.hpp
@@ -0,0 +1,22 @@
+/**
+ * @ Author: Gl.tats
+ * @ Create Time: 2023-12-21 16:17:33
+ * @ Modified by: Gl.tats
+ * @ Modified time: 2024-01-03 16:34:29
+ * @ Description: webserv
+ */
+
+#pragma once
+
+#include <iostream>
+
+class Parsing {
+	private:
+	protected:
+	public:
+		Parsing();
+		Parsing();
+		Parsing(const Parsing &copy);
+		Parsing &operator=(const Parsing &copy);
+		~Parsing();
+}
---
+++ src/Parsing.cpp
@@ -0,0 +1,30 @@
+/**
+ * @ Author: Gl.tats
+ * @ Create Time: 2023-12-21 16:17:24
+ * @ Modified by: Gl.tats
+ * @ Modified time: 2024-01-03 16:46:20
+ * @ Description: webserv
+ */
+
+#include "Parsing.hpp"
+
+//Default constructor
+Parsing::Parsing()
+{}
+
+// Copy constructor
+Parsing(const Parsing &copy)
+{
+	*this = copy;
+}
+
+//copy assignment overload
+Parsing &Parsing::operator=(const Bureaucrat &copy) {
+	if (this == &copy)
+		return *this;
+}
+
+//Destructor
+Parsing::Parsing()
+{}
+
